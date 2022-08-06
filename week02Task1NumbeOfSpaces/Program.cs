b96cd3a87a3d0edd96a7493379f60a23de3892dc
JavidAlakbarov <alakbarovjavid07@gmail.com>
2022-08-07 02:00:21 +0400

    week02task01

---
+++ week02Task1NumbeOfSpaces/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week02Task1NumbeOfSpaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countl = 0, countt = 0, count = 0;
+
+            foreach (char c in "This is a test string")
+            {
+                countt++;
+                if (char.IsLetter(c))
+                {
+                    countl++;
+                }
+            }
+            count = countt - countl;
+            Console.WriteLine("No. of spaces are: " + count);
+        }
+    }
+}
